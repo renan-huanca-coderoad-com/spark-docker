f0de799100ed577bbc51b02996f26c6a5d26ce7a
rhuanca <rhuanca@gmail.com>
2016-08-28 09:47:52 -0400

    starting to have one single start script

--- slave/download-spark.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-url="http://d3kbcqa49mib13.cloudfront.net/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz"
-wget -q "${url}" -O "/tmp/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz"
